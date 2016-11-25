6f4e49c61f1a1dc6062fe0a51bfd21059d863ad2
Elvis Tapfumanei <elvis@elmunei.net>
2016-11-25 16:49:09 +0200

    1st Stage

--- Miracle Pill/ViewController.swift
+++ Miracle Pill/ViewController.swift
@@ -15,7 +15,20 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     
     @IBOutlet weak var provincePickerButton: UIButton!
     
-    let provinces = ["The Eastern Cape",  "The Free State", "Gauteng", "KwaZulu-Natal", "Limpopo", "Mpumalanga", "The Northern Cape", "North West", "The Western Cape"]
+    @IBOutlet weak var countryLabel: UILabel!
+    
+    @IBOutlet weak var countryTextField: UITextField!
+    
+    @IBOutlet weak var postalCodelabel: UILabel!
+    
+    @IBOutlet weak var postalCodeTextField: UITextField!
+    
+    
+    //MARK: Declarations
+    
+    let provinces = ["Eastern Cape",  "Free State", "Gauteng", "KwaZulu-Natal", "Limpopo", "Mpumalanga", "Northern Cape", "North West", "Western Cape"]
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,10 +41,27 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         // Dispose of any resources that can be recreated.
     }
 
+    
+    
+    //MARK: Actions
+    
     @IBAction func provinceButtonPressed(_ sender: UIButton) {
         provinceSelector.isHidden = false
+        countryLabel.isHidden = true
+        countryTextField.isHidden = true
+        postalCodelabel.isHidden  = true
+        postalCodeTextField.isHidden = true
+    }
+    
+    
+    @IBAction func buyNowButton(_ sender: Any) {
+        
         
     }
+    
+    
+    
+    //MARK: Functions & Methods
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
@@ -48,6 +78,10 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         provincePickerButton.setTitle(provinces[row], for: UIControlState.normal)
         provinceSelector.isHidden = true
+        countryLabel.isHidden = false
+        countryTextField.isHidden = false
+        postalCodelabel.isHidden  = false
+        postalCodeTextField.isHidden = false
     }
 }
 
