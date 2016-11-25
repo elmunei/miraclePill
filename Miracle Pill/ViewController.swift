95cd78256d738ce769d840ec25ca37707373cfb9
Elvis Tapfumanei <elvis@elmunei.net>
2016-11-25 12:34:07 +0200

    Init

--- Miracle Pill/ViewController.swift
+++ Miracle Pill/ViewController.swift
@@ -8,11 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    
+    @IBOutlet weak var provinceSelector: UIPickerView!
+    
+    @IBOutlet weak var provincePickerButton: UIButton!
+    
+    let provinces = ["The Eastern Cape",  "The Free State", "Gauteng", "KwaZulu-Natal", "Limpopo", "Mpumalanga", "The Northern Cape", "North West", "The Western Cape"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        provinceSelector.dataSource = self
+        provinceSelector.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func provinceButtonPressed(_ sender: UIButton) {
+        provinceSelector.isHidden = false
+        
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
 
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return provinces.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return provinces[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        provincePickerButton.setTitle(provinces[row], for: UIControlState.normal)
+        provinceSelector.isHidden = true
+    }
 }
 
